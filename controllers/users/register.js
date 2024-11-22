4451adc7d9d23a164b8dac1932a16528d01f08dc
carlosafernandezv <krlosafernandez@gmail.com>
2024-11-22 16:41:31 -0500

    :(

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,18 +1,38 @@
 import User from "../../models/User.js";
-
+import City from "../../models/City.js";
 
 let create = async (req,res,next) =>{
     try {
-        let user = req.body
-        console.log(user);
-        user.online = false
-        
-        let all = await User.create(user)
-        return res.status(201).json({
-            response: all
-        })
+      const { firstName, lastName, email, password, city, photo } = req.body;
+
+      // Verificar si la ciudad existe
+      const cityRecord = await City.findById(city);
+      if (!cityRecord) {
+        return res.status(404).json({ message: "Ciudad no encontrada" });
+      }
+
+      // Crear el nuevo usuario
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+        city: cityRecord._id, // Asociar por ID
+        photo,
+      });
+
+      await newUser.save();
+
+      // Buscar al usuario y poblar el campo "city"
+      const userWithCity = await User.findById(newUser._id).populate("city");
+
+      res.status(201).json({
+        message: "Usuario registrado exitosamente",
+        user: userWithCity, // Incluye todos los datos del usuario + ciudad
+      });
     } catch (error) {
-        next(error)
+      console.error("Error en el registro:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ import { Schema, model } from "mongoose";
 
 let collection = "users";
 let schema = new Schema({
-    name: { type: String, required: true },
-    lastname: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
     photo:{ type: String, required:true},
